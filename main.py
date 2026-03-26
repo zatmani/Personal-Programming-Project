27f8230f37cdcde4517589d27a6310513b31185d
zatmani <zatmani@connect.kellettschool.com>
2026-03-26 11:17:34 +0800

    I completed the start of my code

--- main.py
+++ main.py
@@ -16,17 +16,25 @@ print(f"Ok... Preparing game for {num_players} players")
 print("Shuffling cards...")
 player_1_m = 500
 player_2_m = 500
-player_1_c = random.sample(cards, 2) 
-player_2_c = random.sample(cards, 2) 
+players_cards = []
+player_1_c = random.sample(cards, 2)
+players_cards += [player_1_c]
+player_2_c = random.sample(cards, 2)
+players_cards += [player_2_c]
 print(f"Player 1 has {player_1_m}$")
 print(f"Player 2 has {player_2_m}$")
-players_cards = []
-players_cards += [player_1_c, player_2_c]
+cards.remove(player_1_c[0])
+cards.remove(player_1_c[1])
+cards.remove(player_2_c[0])
+cards.remove(player_2_c[1])
+
 if num_players == 3:
     player_3_m = 500
-    print(f"Player 3 has {player_3_m}$")
     player_3_c = random.sample(cards, 2)
     players_cards += [player_3_c]
+    print(f"Player 3 has {player_3_m}$")
+    cards.remove(player_3_c[0])
+    cards.remove(player_3_c[1])
 if num_players == 4:
     player_3_m = 500
     player_4_m = 500
@@ -34,9 +42,15 @@ if num_players == 4:
     print(f"Player 4 has {player_4_m}$")
     player_3_c = random.sample(cards, 2)
     player_4_c = random.sample(cards, 2)
-    players_cards += player_4_c
+    players_cards += [player_3_c]
+    players_cards += [player_4_c]
+    cards.remove(player_3_c[0])
+    cards.remove(player_3_c[1])
+    cards.remove(player_4_c[0])
+    cards.remove(player_4_c[1])
 num = 1
 card = 0
+input("Ok, I will show each player their cards starting with player 1. Enter something when you are ready to see your cards.")
 for i in range(num_players):
     print(f"Player {num}, I will show you your cards now")
     print("Other players look away now")
@@ -45,8 +59,16 @@ for i in range(num_players):
         sleep(1)
     print(players_cards[card])
     yes_no = input("Enter something when you have memorized your cards.\n")
-
     system("cls")
     num +=1
     card += 1
-    
+print("Now we can start playing!")
+community_cards = []
+community_cards = random.sample(cards, 3)
+cards.remove(community_cards[0])
+cards.remove(community_cards[1])
+cards.remove(community_cards[2])
+print("Here are your guys' first 3 cards.")
+print(community_cards)
+
+
