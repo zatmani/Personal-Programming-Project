5c82a1ebdd0423ed67802d295345755f484d11aa
zatmani <zatmani@connect.kellettschool.com>
2026-03-26 01:58:28 +0000

    Mar 26, 2026, 9:58 AM

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ print("I will be your dealer")
 num_players = int(input("How many users will be playing?"))
 while num_players < 2 or num_players > 4:
     print("There must be 2 players minimum and 4 maximum")
+    num_players = int(input("How many users will be playing?"))
 print(f"Ok... Preparing game for {num_players} players")
 print("Shuffling cards...")
 if num_players == 2:
