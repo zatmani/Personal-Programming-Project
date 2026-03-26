2e7db6acf26b890ab5d6a96141d672601f871ea2
zatmani <zatmani@connect.kellettschool.com>
2026-03-26 02:23:32 +0000

    Mar 26, 2026, 10:23 AM

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ import random
 
 
 
-
-
-
-
-
 cards = ['A♡', 'K♡', 'Q♡', 'J♡', 'X♡', '9♡', '8♡', '7♡', '6♡', '5♡', '4♡', '3♡', '2♡', 'A♧', 'K♧', 'Q♧', 'J♧', 'X♧', '9♧', '8♧', '7♧', '6♧', '5♧', '4♧', '3♧', '2♧', 'A♢', 'K♢', 'Q♢', 'J♢', 'X♢', '9♢', '8♢', '7♢', '6♢', '5♢', '4♢', '3♢', '2♢', 'A♤', 'K♤', 'Q♤', 'J♤', 'X♤', '9♤', '8♤', '7♤', '6♤', '5♤', '4♤', '3♤', '2♤',]
 print("♣️Welcome to Poker!♣️")
 print("I will be your dealer")
@@ -30,14 +25,14 @@ players_cards += [player_1_c, player_2_c]
 if num_players == 3:
     player_3_m = 500
     print(f"Player 3 has {player_3_m}$")
-    player_3_c = random.choice(cards, 2)
+    player_3_c = random.sample(cards, 2)
 if num_players == 4:
     player_3_m = 500
     player_4_m = 500
     print(f"Player 3 has {player_3_m}$")
     print(f"Player 4 has {player_4_m}$")
-    player_3_c = random.choice(cards, 2)
-    player_4_c = random.choice(cards, 2)
+    player_3_c = random.sample(cards, 2)
+    player_4_c = random.sample(cards, 2)
 num = 1
 card = 0
 for i in range(num_players):
@@ -46,6 +41,6 @@ for i in range(num_players):
     for i in range(5, -1, -1):
         print(i)
         sleep(1)
-        print(players_cards[card])
+    print(players_cards[card])
     system("cls")
     num +=1
